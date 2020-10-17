e7cec506cbb24b2f616523e61a7bb5f00a7987f7
ajithbackyard <56559326+ajithbackyard@users.noreply.github.com>
2020-10-17 10:09:05 +0530

    Create gassensingandalerting.ino

---
+++ gassensingandalerting.ino
@@ -0,0 +1,54 @@
+//copy past this code to arduino uno or arduinoDriod app
+int LED1 = 11;
+int buzzer = 8;
+int smokeA0 = A5;
+int sensorThreshold =85;
+int sensor = 7;
+int LED2 = 10;
+
+
+
+void setup() {
+  
+  pinMode(LED1, );
+  pinMode(LED2, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(smokeA0, INPUT);
+  pinMode(sensor, INPUT);
+  Serial.begin(9600);
+  digitalWrite(sensor,LOW);
+}
+
+void loop() {
+  int analogSensor = analogRead(smokeA0);
+
+  Serial.print("Pin A0: ");
+  Serial.println(analogSensor);
+  // Checks if it has reached the threshold value
+  if (analogSensor > sensorThreshold )
+  {
+    digitalWrite(LED1, HIGH);
+     digitalWrite(LED2, LOW);
+     tone(buzzer, 100);
+  
+  if ( digitalRead(sensor)==HIGH )
+  {
+    
+    digitalWrite(LED1, HIGH);
+    tone(buzzer, 500);
+     digitalWrite(LED2, HIGH);
+    
+     
+  }
+  }
+   
+  else
+  {
+    
+    digitalWrite(LED1, LOW);
+    noTone(buzzer);
+     digitalWrite(LED2, LOW);
+    }
+  delay(100);
+}
+
